51acc1d35d485b94de6f1e6fa4749e7627256bc7
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2014-02-17 13:16:02 +0100

    LeagueId as a query parameter.

--- KMorcinek.ShowMyHaxballGames/IndexModule.cs
+++ KMorcinek.ShowMyHaxballGames/IndexModule.cs
@@ -8,14 +8,21 @@ namespace KMorcinek.ShowMyHaxballGames
     {
         public IndexModule()
         {
-            Get["/"] = _ =>
+            Get["/"] = _ => @"This website make it easier to find the people we haven't played so far (or check all results by a player). 
+                                <br />
+                                Try 'yourLeagueId/yourName' i.ee '/121729/Marian' or click <a href='/121729/Marian'>/121729/Marian</a>
+                                <br />
+                                Loading takes time (I am not caching, to serve always up to date results).";
+
+            Get["/{leagueId}"] = parameters =>
             {
-                return View["Index"];
+                var leagueId = int.Parse(parameters.leagueId.Value);
+                return View["Index", leagueId];
             };
 
-            Get["/{name}"] = parameters =>
+            Get["/{leagueId}/{name}"] = parameters =>
             {
-                var leagueId = 126004;
+                var leagueId = int.Parse(parameters.leagueId.Value);
                 var name = parameters.name.Value as string;
                 var gamesViewModelFactory = new GamesViewModelFactory();
                 var gamesViewModel = gamesViewModelFactory.Create(leagueId, name);
